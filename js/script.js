a49e2c95acb3700bdaaa0a603b41094f961dcfbc
David Herbert <daviehert@gmail.com>
2020-08-25 23:47:56 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('blue')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = './css/style.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = './css/blue-theme.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = './css/green-theme.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = './css/purple-theme.css'
+  }
+  
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = './css/dark-theme.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
